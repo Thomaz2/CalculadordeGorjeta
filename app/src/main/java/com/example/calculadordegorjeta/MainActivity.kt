3583751e9ff48dcadad942863c773106034397c6
Thomaz2 <thomaz.fm02@gmail.com>
2023-11-07 11:28:50 -0300

    teste1

--- app/src/main/java/com/example/calculadordegorjeta/MainActivity.kt
+++ app/src/main/java/com/example/calculadordegorjeta/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.calculadordegorjeta
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,7 +28,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,44 +50,101 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Preview(showSystemUi = true)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppCalculadorDeGorjeta(){
+fun AppCalculadorDeGorjeta() {
     var totalConta by remember { mutableStateOf("") }
-    val valorGorjeta = calcularGorjeta(totalConta.toDoubleOrNull()?:0.0)
+    var porcentagem by remember { mutableStateOf("") }
+    var arredondar by remember { mutableStateOf(false) }
+    val valorGorjeta = calcularGorjeta(
+        totalConta.toDoubleOrNull() ?: 0.0,
+        porcentagem.toDoubleOrNull() ?: 15.0,
+        arredondar
+    )
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         Text(
-            text = "Calculador de Gorjeta"
+            text = stringResource(R.string.calculador_de_gorjeta)
         )
-        TextField(
+        campoDeTexto(
             value = totalConta,
-            onValueChange ={totalConta = it},
-            label = {
-                Text(text = "Valor da conta")
-            },
-            isError = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Decimal
-            ),
-            keyboardActions =
-            KeyboardActions()
-
-
+            onValueChange = { totalConta = it },
+            idText = R.string.valor_da_conta,
+            imeAction = ImeAction.Next
         )
-        Spacer(
-            modifier = Modifier.size(10.dp)
+
+        campoDeTexto(
+            value = porcentagem,
+            onValueChange = { porcentagem = it },
+            idText = R.string.gorjeta,
+            imeAction = ImeAction.Done
         )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+
+        ){
+            Text(
+                text = "Arredondar Gorjeta",
+            )
+            Switch(
+                checked = arredondar,
+                onCheckedChange = { arredondar = it }
+            )
+        }
         Text(
             text = stringResource(R.string.valor_gorjeta, valorGorjeta)
         )
     }
 }
-fun calcularGorjeta(totalConta:Double, porcentagemGorjeta:Double = 15.0):String{
-    val gorjeta = porcentagemGorjeta / 100 * totalConta
+
+fun calcularGorjeta(
+    totalConta: Double,
+    porcentagemGorjeta: Double = 15.0 ,
+    arredondar :Boolean= false
+): String {
+    var gorjeta = porcentagemGorjeta / 100 * totalConta
+    if (arredondar){
+        gorjeta= kotlin.math.ceil(gorjeta)
+    }
     return NumberFormat.getCurrencyInstance().format(gorjeta)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun campoDeTexto(
+    value: String,
+    onValueChange: (String) -> Unit,
+    @StringRes idText: Int,
+    imeAction: ImeAction,
+    @DrawableRes iconeCAmpoTexto : Int
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        leadingIcon = { Icon(painter = painterResource(id = iconeCAmpoTexto)) },
+        label = {
+            Text(text = stringResource(id = idText))
+        },
+        isError = false,
+        shape = RectangleShape,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = imeAction
+        ),
+    )
+    Spacer(
+        modifier = Modifier.size(10.dp)
+    )
+
 }
